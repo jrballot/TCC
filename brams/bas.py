ba8b849a7c74ac79182c29b253d26a8adaae5557
Julio Rangel Ballot <julio.ballot@gmail.com>
2017-03-20 09:37:45 -0300

    Finalizando TCC

--- brams/bas.py
+++ brams/bas.py
@@ -39,7 +39,7 @@ def preRun():
 
     if not os.path.exists(_pathIMG):
         os.makedirs(_pathIMG)
-    
+
     if not os.path.exists(_pathIMG2):
         os.makedirs(_pathIMG2)
 
@@ -94,15 +94,15 @@ def grib2dp():
 
 def copyDP():
     "copyDP - Copy DP files to your proper destination inside BRAMS's hierarchy directories"
-    
+
 
     patternDP = "dp\d\d\d\d\-\d\d-\d\d-\d\d\d\d"
     regexDP = re.compile(patternDP)
 
     if os.path.exists(_meteo_only_datain):
-      
+
         try:
-            print "Copying DP files"    
+            print "Copying DP files"
             for f in os.listdir(_pathREGIONAL):
                 if re.search(regexDP, f):
                     shutil.copy(os.path.join(_pathREGIONAL, f), _meteo_only_datain)
@@ -123,8 +123,6 @@ def runModel():
     """
 
     runStages = ['RAMSIN_MAKESFC',"RAMSIN_MAKEVFILE", "RAMSIN_INITIAL"]
-    
-    commandRunModel = 'mpirun -np 1 ../bin/brams-5.2'
 
     for runStage in runStages:
         try:
@@ -141,28 +139,29 @@ def getOutputInPNG(time, date, modelid):
     """
 
     _modelOutputPath = os.path.join(_pathIMG2, modelid)
-    
+
     if not os.path.exists(_modelOutputPath):
         os.makedirs(_modelOutputPath)
-    
+
     year = date[:4]
     month = date[4:6]
     day = date[-2:]
-    
-    file = os.path.join(_meteo_only,'dataout/POSPROCESS/METEO-ONLY-A-'+year+'-'+month+'-'+day+'-'+time+'0000-g1.ctl')
+
+    file = os.path.join(_meteo_only,
+        'dataout/POSPROCESS/METEO-ONLY-A-'+year+'-'+month+'-'+day+'-'+time+'0000-g1.ctl')
     ga = grads.GrADS(Window=False)
     fh = ga.open(file)
-    
+
     # Temperatura
     ga.contour(ga.expr('tempk')-274.15)
     title('Temperatura {} dia {}/{}/{} as {}h'.format(fh.title,day,month,year,time))
     savefig(os.path.join(_modelOutputPath,fh.title+'-temperatura-'+date+time+'.png'))
-    
+
     # Precipitacao
     ga.contour('rh')
     title('Precipitacao {} dia {}/{}/{} as {}h'.format(fh.title,day,month,year,time))
     savefig(os.path.join(_modelOutputPath,fh.title+'-precipitacao-'+date+time+'.png'))
-    
+
     pass
 
 def main(date, time, modelid):
@@ -198,5 +197,5 @@ if __name__ == "__main__":
     result = parser.parse_args()
 
     #print "Initial date: {}\nInitial time: {}".format(result.initdate, result.inittime)
-    
+
     main(result.initdate, result.inittime, result.modelid)
